de06dddf415f2f0fce493c6f3dcaf818750d7414
unknown <Afofou Yimdji>
2025-12-21 16:09:47 +0100

    debug: add diagnostic route to check database connection

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -9,3 +9,16 @@ Route::get('/', function () {
 Route::get('/health', function () {
     return 'OK';
 });
+
+Route::get('/debug', function () {
+    return [
+        'connection' => config('database.default'),
+        'mysql_config' => [
+            'host' => config('database.connections.mysql.host'),
+            'database' => config('database.connections.mysql.database'),
+            'username' => config('database.connections.mysql.username'),
+        ],
+        'env_db_host' => env('DB_HOST'),
+        'tables' => \Illuminate\Support\Facades\DB::connection()->select('SHOW TABLES'),
+    ];
+});
